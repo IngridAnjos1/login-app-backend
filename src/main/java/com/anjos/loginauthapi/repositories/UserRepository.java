744c6905f6ea04b84970c63838de0d0e4126ea07
Ingrid <ingrid_anjos@hotmail.com>
2024-04-09 22:24:54 -0300

    feat: criacao da do metodo findByEmail

--- src/main/java/com/anjos/loginauthapi/repositories/UserRepository.java
+++ src/main/java/com/anjos/loginauthapi/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.anjos.loginauthapi.repositories;
 import com.anjos.loginauthapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+  Optional<User> findByEmail(String email);
 }
